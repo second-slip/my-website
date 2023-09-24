78be815ba101b49d5e36d299aecc8bc059e2526b
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-09-24 10:39:13 +0100

    Miscellaneous small refactoring changes

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
@@ -8,7 +9,8 @@ describe('AboutComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AboutComponent]
+      declarations: [AboutComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     });
     fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -9,6 +9,9 @@ import { MatButtonHarness } from '@angular/material/button/testing';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Router, Routes } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BlogComponent } from '../blog/blog.component';
 
 // let loader: HarnessLoader;
 
@@ -38,15 +41,20 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 describe('my-component', () => {
   let fixture: ComponentFixture<MenuComponent>;
   let loader: HarnessLoader;
+  let router: Router;
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule(
       {
-        imports: [MatButtonModule, MatToolbarModule],
+        imports: [MatButtonModule, MatToolbarModule, RouterTestingModule.withRoutes([
+          { path: 'login', component: BlogComponent },
+        ])],
         declarations: [MenuComponent],
-        schemas: [NO_ERRORS_SCHEMA],
+        schemas: [NO_ERRORS_SCHEMA]
       })
       .compileComponents();
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(MenuComponent);
     loader = TestbedHarnessEnvironment.loader(fixture);
   });
@@ -58,5 +66,34 @@ describe('my-component', () => {
     // icon button not displayed on small screens, so test 4 ?
 
     const firstButton = await loader.getHarness(MatButtonHarness); // === buttons[0]
+  });
+
+  it('render an active title button button', async () => {
+    const titleButton = await loader.getHarness(MatButtonHarness.with({selector: '.title-button'}));
+    //expect(fixture.componentInstance.confirmed).toBe(false);
+    expect(await titleButton.isDisabled()).toBe(false);
+    expect(await titleButton.getText()).toBe('andrewstuartcross.co.uk');
+    //await blogLink.click();
+    //expect(fixture.componentInstance.confirmed).toBe(true);
+  });
+
+  it('should render an active Home button', async () => {
+    const homeLink = await loader.getHarness(MatButtonHarness.with({ text: 'Home' }));
+    expect(await homeLink.isDisabled()).toBe(false);
+  });
+
+  it('should render an active Blog button', async () => {
+    const blogLink = await loader.getHarness(MatButtonHarness.with({ text: 'Blog' }));
+    expect(await blogLink.isDisabled()).toBe(false);
+  });
+
+  it('should render an active About button', async () => {
+    const aboutLink = await loader.getHarness(MatButtonHarness.with({ text: 'About' }));
+    expect(await aboutLink.isDisabled()).toBe(false);
+  });
+
+  it('should render an active GitHub icon button', async () => {
+    const gitHubIconButton = await loader.getHarness(MatButtonHarness.with({ selector: '.github-icon-button' }));
+    expect(await gitHubIconButton.isDisabled()).toBe(false);
   });
 });
