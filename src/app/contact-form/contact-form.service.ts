cecdf7c52950530c2d58d51b7e50bf7435fbba29
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-09-24 10:36:11 +0100

    ContactFormService imp & tests; test helpers added

---
+++ src/app/contact-form/contact-form.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IContactForm } from './i-contact-form.dto';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactFormService {
+
+  constructor(private readonly _http: HttpClient) { }
+
+  public postContactForm(model: IContactForm): Observable<{ success: boolean }> {
+    return this._http.post<{ success: boolean }>('api/.................', model, httpOptions);
+  }
+}
