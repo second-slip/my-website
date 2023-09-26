03fb8875fbd45522191d646438325959c5cc5dad
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-09-26 17:41:38 +0100

    Trying to get CORS configured

--- src/app/contact-form/contact-form.service.ts
+++ src/app/contact-form/contact-form.service.ts
@@ -3,9 +3,9 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IContactForm } from './i-contact-form.dto';
 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
+// const httpOptions = {
+//   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+// };
 
 const url = 'https://birder-server.azurewebsites.net/api/message/send-contact-message'
 
@@ -17,6 +17,13 @@ export class ContactFormService {
   constructor(private readonly _http: HttpClient) { }
 
   public postContactForm(model: IContactForm): Observable<{ success: boolean }> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    httpOptions.headers.set("Access-Control-Allow-Origin", "*");
+
+
     return this._http.post<{ success: boolean }>(url, model, httpOptions);
   }
 }
