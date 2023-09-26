849b41415ec211de6fbc9927a12625a472920e1e
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-09-26 17:13:23 +0100

    Update url

--- src/app/contact-form/contact-form.service.ts
+++ src/app/contact-form/contact-form.service.ts
@@ -7,6 +7,8 @@ const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
+const url = 'https://birder-server.azurewebsites.net/api/message/send-contact-message'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,6 +17,6 @@ export class ContactFormService {
   constructor(private readonly _http: HttpClient) { }
 
   public postContactForm(model: IContactForm): Observable<{ success: boolean }> {
-    return this._http.post<{ success: boolean }>('api/.................', model, httpOptions);
+    return this._http.post<{ success: boolean }>(url, model, httpOptions);
   }
 }
