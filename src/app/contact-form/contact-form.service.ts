f85f346beb957a13ae71a9dc6aaf4a747d9ece43
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-09-26 19:01:46 +0100

    reset req headers

--- src/app/contact-form/contact-form.service.ts
+++ src/app/contact-form/contact-form.service.ts
@@ -5,7 +5,7 @@ import { IContactForm } from './i-contact-form.dto';
 
 const httpOptions = {
   headers: new HttpHeaders(
-    { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+    { 'Content-Type': 'application/json' },
     )
 };
 
