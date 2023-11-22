469497afb9eb6f7a3b43a365e23cba92919c5083
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-11-22 11:41:15 +0000

    Fix unit tests for standalone API setup

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -3,15 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AboutComponent } from './about.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
+import { MockComponent } from 'ng-mocks';
+import { ContactFormComponent } from '../contact-form/contact-form.component';
+
 describe('AboutComponent', () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [AboutComponent],
-    schemas: [NO_ERRORS_SCHEMA]
-});
+      imports: [AboutComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+      .overrideComponent(AboutComponent, {
+        remove: { imports: [ContactFormComponent] },
+        add: { imports: [MockComponent(ContactFormComponent)] }
+      });
     fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
-    standalone: true,
-    imports: [RouterLink]
+    standalone: true
 })
 export class HomeComponent { }
