852df27347d3f55aeae759c666175c67f945f935
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-11-23 11:19:03 +0000

    add missing RouterLink import

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './home.component'
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [HomeComponent]
+    imports: [HomeComponent, RouterTestingModule]
 });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
-    standalone: true
+    standalone: true,
+    imports: [RouterLink]
 })
 export class HomeComponent { }
