80d6559d2c4c87960215c59f5a2089b365cb5ea0
Andrew Cross <andrew-stuart-cross@outlook.com>
2024-06-03 14:37:50 +0100

    Updated to zoneless

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -9,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient()
+    provideHttpClient(),
+    provideExperimentalZonelessChangeDetection(),
   ]
 };
