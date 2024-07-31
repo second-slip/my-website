fa5d7fca34d1c7cc0220f968e8eb9bfbe2f5e314
Andrew Cross <andrew-stuart-cross@outlook.com>
2024-07-31 12:35:17 +0100

    Fix navbar responsive padding

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -7,9 +7,9 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
+    provideExperimentalZonelessChangeDetection(),
     provideAnimations(),
-    provideHttpClient(),
-    provideExperimentalZonelessChangeDetection()
+    provideHttpClient()
   ]
 };
