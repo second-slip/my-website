03ea02167dbbd4efd642afa2b10f169f14b88666
Andrew Cross <andrew-stuart-cross@outlook.com>
2025-01-15 20:02:51 +0000

    host element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    imports: [NavBarComponent, RouterOutlet, FooterComponent, NavBarComponent]
+  selector: 'app-root',
+  // host: { class: 'std-padding' },
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  imports: [NavBarComponent, RouterOutlet, FooterComponent, NavBarComponent],
 })
 export class AppComponent {
   title = 'A S Cross';
